ae5f6d3e4cd1a90c7de1f4668d88e88ef8cadd5a
Alex Arriaga <alexzander.arriaga@base22.com>
2019-01-14 09:42:45 -0600

    Corregir componente de creación de libros

--- pages/books/create.js
+++ pages/books/create.js
@@ -13,6 +13,9 @@ class CreateBookPage extends React.Component {
 
   handleOnChange = (event) => {
     const {name, value} = event.target;
+
+    console.log(name, value);
+    console.log(this.state);
     this.setState({[name]: value});
   };
 
@@ -46,14 +49,14 @@ class CreateBookPage extends React.Component {
 
                         <div className="form-group">
                           <label htmlFor="title">Title</label>
-                          <input type="text" className="form-control" id="title"
+                          <input type="text" className="form-control" id="title" name="title"
                                  placeholder="Enter title" defaultValue={this.state.title}
                                  onChange={this.handleOnChange}/>
                         </div>
 
                         <div className="form-group">
                           <label htmlFor="description">Description</label>
-                          <textarea rows="8" className="form-control" id="description"
+                          <textarea rows="8" className="form-control" id="description" name="description"
                                     placeholder="Enter description"
                                     defaultValue={this.state.description}
                                     onChange={this.handleOnChange}/>
